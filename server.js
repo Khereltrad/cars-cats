85a57b1aa0dc7b98a56b43188ff427248e8a9ef8
Claudio René Aránguiz Herrera <claudioarnguiz@yahoo.com>
2021-02-02 10:18:11 -0300

    commit avance tarea EJS cars and cats

--- server.js
+++ server.js
@@ -2,14 +2,32 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//^ esta es la línea que le dice a nuestro servidor que use la carpeta "/ static" para contenido estático
 app.use(express.static(__dirname+"/public"));
+
+//? EJS
+//^ Esto establece la ubicación donde express buscará la vista ejs
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
 
 app.get("/cats",(req,res) =>{
     res.render("gatos");
 });
 
 app.get("/cars",(req,res) =>{
-    res.render("vehiculos");
+    res.render("cars");
 });
 
+app.get("/cars/new",(req,res) =>{
+    res.render("form");
+});
+
+app.get("/detalle",(req,res) =>{
+    res.render("detalle");
+});
+
+
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
+
